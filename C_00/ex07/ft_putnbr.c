4c610141cd22c0bfac9b0b49d33d93e5afbbeeff
Yassine Zaoui <yzaoui@e3r8p6.clusters.42paris.fr>
2022-08-16 16:28:20 +0200

    Envoie 0 du C00

--- C_00/ex07/ft_putnbr.c
+++ C_00/ex07/ft_putnbr.c
@@ -6,16 +6,34 @@
 /*   By: yzaoui <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 13:47:43 by yzaoui            #+#    #+#             */
-/*   Updated: 2022/08/12 14:20:48 by yzaoui           ###   ########.fr       */
+/*   Updated: 2022/08/16 12:46:17 by yzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
+int	particular_case(int nbr)
+{
+	if (nbr == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (1);
+	}
+	else if (nbr == 2147483647)
+	{
+		write(1, "2147483647", 10);
+		return (1);
+	}
+	else
+		return (0);
+}
+
 void	ft_putnbr(int nbr)
 {
 	char	a;
 
+	if (particular_case(nbr))
+		return ;
 	if (nbr / 10 == 0)
 	{
 		a = nbr + 48;
