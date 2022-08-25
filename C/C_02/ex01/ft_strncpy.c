8a0354bba0a96b9c02dba88c006700fb65577a15
Yassine Zaoui <yzaoui@e2r10p15.clusters.42paris.fr>
2022-08-25 04:18:20 +0200

    2.0 C02

--- C/C_02/ex01/ft_strncpy.c
+++ C/C_02/ex01/ft_strncpy.c
@@ -6,18 +6,19 @@
 /*   By: yzaoui <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 13:23:31 by yzaoui            #+#    #+#             */
-/*   Updated: 2022/08/25 02:02:52 by yzaoui           ###   ########.fr       */
+/*   Updated: 2022/08/25 04:13:59 by yzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+//#include <stdio.h>
+//#include <string.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (i < n && src[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
@@ -34,6 +35,10 @@ int	main(void)
 {
 	char	src[] = "yaya bizarre adventure";
 	char	dest[] = "77777777777777777777777777";
-	printf("%s",ft_strncpy(dest, src, 100));
+	unsigned int	i;
+
+	i = 50;
+	printf("%s\n",ft_strncpy(dest, src, i));
+	printf("%s",strncpy(dest, src, i));
 	return (0);
 }*/
--- C/C_02/ex10/ft_strlcpy.c
+++ C/C_02/ex10/ft_strlcpy.c
@@ -6,35 +6,46 @@
 /*   By: yzaoui <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 17:23:04 by yzaoui            #+#    #+#             */
-/*   Updated: 2022/08/25 02:08:53 by yzaoui           ###   ########.fr       */
+/*   Updated: 2022/08/25 04:17:11 by yzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
-	unsigned int	j;
 
-	j = 0;
 	i = 0;
-	while (src[j])
-		j++;
-	while (i < size && src[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (j);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (ft_strlen(src));
 }
 /*
 int	main(void)
 {
-	char	src[] = "yaya bizarre adventure";
-	char	dest[] = "77777777777777777777777777";
-	printf("%d",ft_strlcpy(dest, src, 50));
+	char	src[] = "je suis persuader que ca va bug";
+	char	dest[] = "bonjour";
+	printf("%d\n",ft_strlcpy(dest, src, 6));
+	printf("%s",dest);
 	return (0);
 }
 */
